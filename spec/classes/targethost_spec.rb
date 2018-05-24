6bafd4eb0ce7b0e54ddf511d29c2a35e64c22693
Derek Smart <derek.smart@delphix.com>
2018-05-24 11:38:25 -0700

    fix hash 1.9
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- spec/classes/targethost_spec.rb
+++ spec/classes/targethost_spec.rb
@@ -6,12 +6,12 @@ describe 'delphix::targethost' do
       let(:facts) { os_facts }
       let :params do
         {
-          delphix_user     => 'delphix',
-          delphix_group    => 'delphix',
-          delphix_mount    => '/mnt/delphix',
-          delphix_toolkit  => '/home/delphix/toolkit',
-          delphix_ssh_user => '',
-          delphix_ssh_key  => '',
+          'delphix_user'     => 'delphix',
+          'delphix_group'    => 'delphix',
+          'delphix_mount'    => '/mnt/delphix',
+          'delphix_toolkit'  => '/home/delphix/toolkit',
+          'delphix_ssh_user' => '',
+          'delphix_ssh_key'  => '',
         }
       end
 
