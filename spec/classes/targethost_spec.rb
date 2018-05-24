557648a21ca3a4aba6b6a146d4046ef2572fc84b
Derek Smart <derek.smart@delphix.com>
2018-05-24 11:23:47 -0700

    update rubocop check
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- spec/classes/targethost_spec.rb
+++ spec/classes/targethost_spec.rb
@@ -5,15 +5,16 @@ describe 'delphix::targethost' do
     context "on #{os}" do
       let(:facts) { os_facts }
       let :params do
-      {
-        :delphix_user     => 'delphix',
-        :delphix_group    => 'delphix',
-        :delphix_mount    => '/mnt/delphix',
-        :delphix_toolkit  => '/home/delphix/toolkit',
-        :delphix_ssh_user => '',
-        :delphix_ssh_key  => '',
-      }
+        {
+          delphix_user     => 'delphix',
+          delphix_group    => 'delphix',
+          delphix_mount    => '/mnt/delphix',
+          delphix_toolkit  => '/home/delphix/toolkit',
+          delphix_ssh_user => '',
+          delphix_ssh_key  => '',
+        }
       end
+
       it { is_expected.to compile }
     end
   end
